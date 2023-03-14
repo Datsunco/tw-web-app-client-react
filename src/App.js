16f2beb59366380f55191fe593d00155c6dd13fc
Datsunco <danya200375d@gmail.com>
2023-03-14 21:42:10 +0300

    butons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {useEffect} from "react";
 import Button from "./components/Button/Button";
+import Header from "./components/Header/Header";
 const tg = window.Telegram.WebApp;
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
         <Button>first</Button>
         <Button>sec</Button>
+        <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from "../Button/Button";
+
+const Header = () => {
+    return (
+        <div className={'header'}>
+            <Button>lol</Button>
+        </div>
+    );
+};
+
+export default Header;
