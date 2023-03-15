f8addb8d325338ac2a5d8f20ef227e91565fceb7
Datsunco <danya200375d@gmail.com>
2023-03-15 19:19:28 +0300

    butons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <div className={'header'}>
             <div className={'token_amount_block'}>
-                <img className={'logo'} src={"https://sun9-33.userapi.com/impg/vzl0gqhFCJzKASoEXdFU0OY9sOW1vbhqYYk54A/GsgGVTmrDCk.jpg?size=287x302&quality=96&sign=a8cbf5305b9ed3ee2c279a9472f86ade&type=album"}/>
+                <img alt={''} className={'logo'} src={"https://sun9-33.userapi.com/impg/vzl0gqhFCJzKASoEXdFU0OY9sOW1vbhqYYk54A/GsgGVTmrDCk.jpg?size=287x302&quality=96&sign=a8cbf5305b9ed3ee2c279a9472f86ade&type=album"}/>
                 <text className={'token_amount'} >2342.01232</text>
             </div>
             <div className={'buttons_block'}>
