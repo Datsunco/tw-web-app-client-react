16963caa50b1123a5e188acc1675206729a37887
Datsunco <danya200375d@gmail.com>
2023-03-16 18:21:40 +0300

    butons

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -4,15 +4,16 @@ import './News.css'
 const News = () => {
     return (
         <div className={'news'}>
-            <text className={'news_text'}>Новости проекта</text>
+            <text className={'news_block_text'}>Новости проекта</text>
             <div className={'container'}>
                 <ul>
-                    <li><text >
+                    <li><text className={'news_text'}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at fringilla est,
                         vitae feugiat felis. Cras faucibus vulputate augue. Sed.
                         </text>
+                        <text> Вчера в 18:45</text>
                     </li>
-                    <li><text href={'#'}>
+                    <li><text className={'news_text'}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at fringilla est,
                         vitae feugiat felis. Cras faucibus vulputate augue. Sed.
                     </text>
