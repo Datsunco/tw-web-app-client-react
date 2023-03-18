31190b62ed6da2726c377a3c46bdd10802cf96de
Datsunco <danya200375d@gmail.com>
2023-03-18 16:16:26 +0300

    donation

--- src/components/Donation/Donation.jsx
+++ src/components/Donation/Donation.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Donation.css'
 
 
 const Donation = () => {
+    const [amount, setAmount] = useState('');
+
+    const onChangeAmount = (e) =>{
+        setAmount(e.target.text)
+    }
+
+
     return (
-        <div>
-            <text>[eq</text>
+        <div className={'donation'}>
+            <text>Вы покупаете</text>
+            <div className={'input_block'}>
+                <input
+                    id={'amount'}
+                    className={'input'}
+                    type="text"
+                    placeholder={'0'}
+                    value={amount}
+                    onChange={onChangeAmount}
+                />
+                <text>TWT</text>
+            </div>
+            <text>1 TWT ≈ 1 rub</text>
         </div>
     );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -50,7 +50,7 @@ const Header = () => {
 
     return (
         <div className={'header'}>
-            <text className={'token_balance_text'}>Баланс Measure</text>
+            <text className={'token_balance_text'}>Баланс TW Token</text>
             <div className={'token_amount_block'}>
                 <img alt={''} className={'logo'}
                      src={"https://sun9-33.userapi.com/impg/vzl0gqhFCJzKASoEXdFU0OY9sOW1vbhqYYk54A/GsgGVTmrDCk.jpg?size=287x302&quality=96&sign=a8cbf5305b9ed3ee2c279a9472f86ade&type=album"}/>
