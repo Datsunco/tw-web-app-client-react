586d1440e7f2c92c65315c38f8778e6f16842720
Datsunco <danya200375d@gmail.com>
2023-03-22 21:40:23 +0300

    services

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -19,20 +19,7 @@ const Map = () => {
         googleMapsApiKey: "AIzaSyD8jQRBkxYYsQb6FWMPNjgSQW1lVIEj1EA"
     })
 
-    const [map, setMap] = React.useState(null);
 
-    const onLoad = React.useCallback(function callback(map) {
-        // This is just an example of getting and using the map instance!!! don't just blindly copy!
-        const bounds = new window.google.maps.LatLngBounds(center);
-        map.fitBounds(bounds);
-
-        setMap(map)
-    }, [])
-
-
-    const onUnmount = React.useCallback(function callback(map) {
-        setMap(null)
-    }, [])
 
     return isLoaded ?(
         <div>
@@ -40,8 +27,7 @@ const Map = () => {
             mapContainerStyle={containerStyle}
             center={center}
             zoom={10}
-            onLoad={onLoad}
-            onUnmount={onUnmount}
+
         >
             { /* Child components, such as markers, info windows, etc. */ }
             <></>
