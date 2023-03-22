a9d59290fb92c5913e4adfe8cda2bb742ff09a4c
Datsunco <danya200375d@gmail.com>
2023-03-22 16:17:36 +0300

    services

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -23,14 +23,15 @@ const Services = () => {
             <div className={'quick_button_block'}>
 
                 <button onClick={onFirButClick} className={'test_button'}>
-                    <img alt={''}  className={'img'}
-                         //src={'https://img-new.cgtrader.com/items/122381/dbaa861cce/photorealistic-earth-and-cloudsat-satellite-3d-model-animated-obj-fbx-c4d-dae.jpg'}
-                    />
                     <text className={'text_button'}>ФОТО ИЗ КОСМОСА</text>
                 </button>
 
                 <button className={'test1_button'}>Скоро</button>
-                <button className={'test2_button'}>Скоро</button>
+
+                    <button className={'test2_button'}>
+                        <text className={'text_button'}>Скоро</text>
+                    </button>
+
 
             </div>
         </div>
