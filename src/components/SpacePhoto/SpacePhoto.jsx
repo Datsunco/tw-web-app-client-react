61fdc57747e1690ed10c117eb500a6da80d1116f
Datsunco <danya200375d@gmail.com>
2023-04-02 15:12:30 +0300

    apiSent

--- src/components/SpacePhoto/SpacePhoto.jsx
+++ src/components/SpacePhoto/SpacePhoto.jsx
@@ -9,8 +9,8 @@ const tg = window.Telegram.WebApp;
 
 const SpacePhoto = () => {
     const [center,setCenter] = useState('');
-    const [lat,setLat] = useState('');
-    const [lon,setLon] = useState('');
+    const [lat,setLat] = useState(0.0);
+    const [lon,setLon] = useState(0.0);
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
         googleMapsApiKey: "AIzaSyD8jQRBkxYYsQb6FWMPNjgSQW1lVIEj1EA"
@@ -20,17 +20,19 @@ const SpacePhoto = () => {
             setCenter(coordinates);
             setLat(coordinates.lat);
             setLon(coordinates.lon);
+            console.log(center)
             console.log(lat)
             console.log(lon);
-        }, [lat,lon])
+        }, [lat,lon,center])
     
     const onSendData = useCallback( () => {
         const data = {
             lat: lat,
             lon: lon,
         }
-        tg.sendData(JSON.stringify(data));
-    }, [lat,lon])
+        console.log(data)
+        tg.sendData(JSON.stringify(center));
+    }, [lat,lon,center])
     
     useEffect(()=>{
         tg.onEvent('mainButtonClicked', onSendData)
